11b0e3fed2614c05000a68cdda2dcb556b6af03b
Saqib <saqibnaseeb14@gmail.com>
2022-07-31 17:30:32 +0500

    routes using net/http

--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type ToDo struct {
+	Title string `json:"Title"`
+	Desc  string `json:"desc"`
+	Id    int    `json:"id"`
+}
+
+// let's declare a global ToDo array
+// that we can then populate in our main function
+// to simulate a database
+var ToDos []ToDo
+
+func returnAllToDos(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(ToDos)
+}
+
+func handleRequests() {
+	http.HandleFunc("/todos", returnAllToDos)
+	log.Fatal(http.ListenAndServe(":1000", nil))
+}
 
 func main() {
-	fmt.Println("hello world")
+	ToDos = []ToDo{
+		ToDo{Title: "Task 1", Desc: "Make UI", Id: 1},
+		ToDo{Title: "Task 2", Desc: "Integrate UI with REST Api", Id: 2},
+	}
+	handleRequests()
 }
